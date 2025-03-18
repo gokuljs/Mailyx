45fc8e0363aaa01b433838c5f692b19a93314ee9
gokuljs <gokuljs029@gmail.com>
2025-03-18 07:08:25 +0530

    signup-redesign

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,34 @@
+import { GlowingEffect } from "@/app/_components/glowing-effect";
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="relative flex h-screen items-center justify-center bg-[#09090b]">
+      <div className="relative flex items-center justify-center">
+        <div className="absolute inset-0">
+          <GlowingEffect
+            blur={2}
+            borderWidth={3}
+            spread={50}
+            glow={true}
+            disabled={false}
+            proximity={80} // Responsive to mouse proximity
+            inactiveZone={0.1}
+          />
+        </div>
+
+        <div className="border-0.75 relative z-10 rounded-md">
+          <SignUp
+            appearance={{
+              baseTheme: [dark],
+              variables: {
+                colorBackground: "#000000",
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
