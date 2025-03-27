7984a49fe024f6593332f0a925b3dd85b9b88c37
gokuljs <gokuljs029@gmail.com>
2025-03-27 18:58:48 +0530

    sign out

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { SignOutButton } from "@clerk/nextjs";
+
 export default async function Home() {
-  return <div className="text-red-400">Hello world</div>;
+  return (
+    <div className="flex items-center justify-between border text-red-400">
+      <div>Hello world</div>
+      <Button>
+        <SignOutButton></SignOutButton>
+      </Button>
+    </div>
+  );
 }
