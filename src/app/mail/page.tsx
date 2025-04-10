d932019c06e5503a36cda9ba26c961ebc32a22b6
gokuljs <gokuljs029@gmail.com>
2025-04-11 02:22:14 +0530

    ui size

--- src/app/mail/page.tsx
+++ src/app/mail/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const page = () => {
-  return <div>Email Dashboard</div>;
+  return (
+    <TooltipProvider delayDuration={0}>
+      <ResizablePanelGroup
+        direction="horizontal"
+        onLayout={(sizes: number[]) => {
+          console.log(sizes);
+        }}
+      ></ResizablePanelGroup>
+    </TooltipProvider>
+  );
 };
 
 export default page;
