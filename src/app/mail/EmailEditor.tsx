b1e9ce3c9fb9c179301d7f6c2e698e791497aa86
gokuljs <gokuljs029@gmail.com>
2025-04-17 15:29:22 +0530

    Email Editor tip tap

--- src/app/mail/EmailEditor.tsx
+++ src/app/mail/EmailEditor.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { EditorContent, useEditor } from "@tiptap/react";
+import StarterKit from "@tiptap/starter-kit";
+import { Text } from "@tiptap/extension-text";
+import EditorMenuBar from "./editorMenuBar";
 
 type Props = {};
 
 const EmailEditor = (props: Props) => {
-  return <div>EmailEditor</div>;
+  const [value, setValue] = useState("");
+  const customText = Text.extend({
+    addKeyboardShortcuts() {
+      return {
+        "Meta-j": () => {
+          console.log("cmd-j");
+          return true;
+        },
+      };
+    },
+  });
+
+  const editor = useEditor({
+    autofocus: false,
+    extensions: [StarterKit, customText],
+    onUpdate: ({ editor }) => {
+      setValue(editor.getHTML());
+    },
+  });
+
+  if (!editor) return <></>;
+
+  return (
+    <div>
+      <div>
+        <EditorMenuBar editor={editor} />
+        <EditorContent editor={editor} value={value} />
+      </div>
+    </div>
+  );
 };
 
 export default EmailEditor;
