79910775cd21da0c5cde1a11b622209a73fba4ca
gokuljs <gokuljs029@gmail.com>
2025-04-18 08:42:47 +0530

    Privacy Policy

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
   "/",
   "/api/clerk/webhook(.*)",
   "/api/initial-sync(.*)",
+  "/privacy(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
