becd3ff5a8024a586fbe815579e3db29ae253ff5
gokuljs <gokuljs029@gmail.com>
2025-04-18 10:00:25 +0530

    bun installation

---
+++ src/app/mail/ai-composer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type Props = {
+  isComposing: boolean;
+  Generate: (token: string) => void;
+};
+
+const AiComposer = (props: Props) => {
+  return <div>AiComposer</div>;
+};
+
+export default AiComposer;
