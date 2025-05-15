ca72380117fcb03e78403b7118cfa15de7b912b2
gokuljs <gokuljs029@gmail.com>
2025-05-15 11:38:53 +0530

    fix

--- src/lib/accounts.ts
+++ src/lib/accounts.ts
@@ -27,7 +27,7 @@ export class Account {
           Authorization: `Bearer ${this.token}`,
         },
         params: {
-          daysWithin: 30,
+          daysWithin: 7,
           bodyType: "html",
         },
       },
