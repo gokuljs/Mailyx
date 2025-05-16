fc853616aed63135f03ebb3d4037238c951237cb
gokuljs <gokuljs029@gmail.com>
2025-05-17 00:49:22 +0530

    input tag fix

--- src/app/mail/TagInput.tsx
+++ src/app/mail/TagInput.tsx
@@ -28,7 +28,7 @@ const TagInput = ({ label, placeholder, onChange, value }: Props) => {
   }));
   return (
     <div className="items flex items-center gap-2 rounded-md border">
-      <span className="ml-3 text-sm text-stone-300">{label}</span>
+      <span className="text-muted-foreground ml-3 text-sm">{label}</span>
       <Select
         onInputChange={setInputValue}
         value={value}
@@ -49,19 +49,19 @@ const TagInput = ({ label, placeholder, onChange, value }: Props) => {
         isMulti
         classNames={{
           menu: () => {
-            return "dark:!bg-[hsl(20_14.3%_4.1%)] !text-white !border !border-muted ";
+            return "bg-background border-border !text-foreground !border";
           },
           option: ({ isFocused }) => {
-            return `${isFocused ? "!bg-muted !text-white" : ""} hover:!bg-muted !text-white !cursor-pointer`;
+            return `${isFocused ? "!bg-accent !text-accent-foreground" : ""} hover:!bg-accent !text-foreground !cursor-pointer`;
           },
           control: () => {
-            return "!border-none !outline-none !ring-0 !shadow-none focus:border-none focus:outline-none focus:ring-0 focus:shadow-none !bg-transparent !dark:bg-transparent";
+            return "!border-none !outline-none !ring-0 !shadow-none focus:border-none focus:outline-none focus:ring-0 focus:shadow-none !bg-transparent";
           },
-          input: () => "!text-white",
-          multiValue: () => "!bg-stone-600 !rounded-md flex items-center",
-          multiValueLabel: () => "!text-white px-2", // no bg here
+          input: () => "!text-foreground",
+          multiValue: () => "!bg-secondary !rounded-md flex items-center",
+          multiValueLabel: () => "!text-secondary-foreground px-2",
           multiValueRemove: () =>
-            "!text-white px-2 hover:!bg-stone-400 hover:!text-white !rounded-r-lg",
+            "!text-secondary-foreground px-2 hover:!bg-destructive hover:!text-destructive-foreground !rounded-r-lg",
         }}
       />
     </div>
